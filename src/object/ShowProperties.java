09b505861ca72f282a44a853391e9b6b9e7310f3
leiziqin <ziqinlei@gmail.com>
2018-06-09 18:38:25 +0800

    add ShowProperties

---
+++ src/object/ShowProperties.java
@@ -0,0 +1,13 @@
+package object;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class ShowProperties {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
