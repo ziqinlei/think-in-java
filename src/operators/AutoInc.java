057babcda6fde2da40a269081385a1bf8423fd96
leiziqin <ziqinlei@gmail.com>
2018-06-09 21:28:04 +0800

    add AutoInc

---
+++ src/operators/AutoInc.java
@@ -0,0 +1,18 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i : " + i);
+        System.out.println("++i : " + ++i);
+        System.out.println("i++ : " + i++);
+        System.out.println("i : " + i);
+        System.out.println("--i : " + --i);
+        System.out.println("i-- : " + i--);
+        System.out.println("i : " + i);
+    }
+}
