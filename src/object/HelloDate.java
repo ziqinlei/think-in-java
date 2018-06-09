ac3fbbc4bd2313b3985df02e8676cd258bbebddb
leiziqin <ziqinlei@gmail.com>
2018-06-09 20:54:49 +0800

    add HelloDate

---
+++ src/object/HelloDate.java
@@ -0,0 +1,18 @@
+package object;
+
+import java.util.Date;
+
+/**
+ * The first Thinking in Java example program.
+ * Displays a string and today's date.
+ *
+ * @author Leiziqin
+ * @version 4.0
+ * @since 2018/6/9
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello it's: ");
+        System.out.println(new Date());
+    }
+}
