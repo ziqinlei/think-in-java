105b11da419285a72c1b030899fdc8ddb54419fa
leiziqin <ziqinlei@gmail.com>
2018-06-09 21:31:18 +0800

    add EqualsMethod

---
+++ src/operators/EqualsMethod.java
@@ -0,0 +1,13 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class EqualsMethod {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1.equals(n2));
+    }
+}
