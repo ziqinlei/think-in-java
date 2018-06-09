99c72392aa49c6da0a37b97c5860c27188a9a356
leiziqin <ziqinlei@gmail.com>
2018-06-09 21:12:29 +0800

    add PassObject

---
+++ src/operators/PassObject.java
@@ -0,0 +1,23 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class PassObject {
+    static void f(Letter y) {
+        y.c = 'z';
+    }
+
+    public static void main(String[] args) {
+        Letter x = new Letter();
+        x.c = 'a';
+        System.out.println("1: x.c: " + x.c);
+        f(x);
+        System.out.println("1: x.c: " + x.c);
+    }
+}
+
+class Letter {
+    char c;
+}
