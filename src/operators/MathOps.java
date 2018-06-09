e156808cb7a6613680c14da5deda68651791ea8b
leiziqin <ziqinlei@gmail.com>
2018-06-09 21:20:24 +0800

    add MathOps

---
+++ src/operators/MathOps.java
@@ -0,0 +1,51 @@
+package operators;
+
+import java.util.Random;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class MathOps {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int i, j, k;
+        j = rand.nextInt(100) + 1;
+        System.out.println("j : " + j);
+        k = rand.nextInt(100) + 1;
+        System.out.println("k : " + k);
+        i = j + k;
+        System.out.println("j + k : " + i);
+        i = j - k;
+        System.out.println("j - k : " + i);
+        i = k / j;
+        System.out.println("k / j : " + i);
+        i = k * j;
+        System.out.println("k * j : " + i);
+        i = k % j;
+        System.out.println("k % j : " + i);
+        j %= k;
+        System.out.println("j %= k : " + j);
+        float u, v, w; // Applies to doubles, too
+        v = rand.nextFloat();
+        System.out.println("v : " + v);
+        w = rand.nextFloat();
+        System.out.println("w : " + w);
+        u = v + w;
+        System.out.println("v + w : " + u);
+        u = v - w;
+        System.out.println("v - w : " + u);
+        u = v * w;
+        System.out.println("v * w : " + u);
+        u = v / w;
+        System.out.println("v / w : " + u);
+        u += v;
+        System.out.println("u += v : " + u);
+        u -= v;
+        System.out.println("u -= v : " + u);
+        u *= v;
+        System.out.println("u *= v : " + u);
+        u /= v;
+        System.out.println("u /= v : " + u);
+    }
+}
