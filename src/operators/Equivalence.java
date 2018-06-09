342609cfa562974c157ec91678247a7449834f7d
leiziqin <ziqinlei@gmail.com>
2018-06-09 21:30:08 +0800

    add Equivalence

---
+++ src/operators/Equivalence.java
@@ -0,0 +1,14 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+    }
+}
