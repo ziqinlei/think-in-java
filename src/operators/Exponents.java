5b7f1303ebc24f4852c6c55e36b4a85cf1ce07e4
leiziqin <ziqinlei@gmail.com>
2018-06-09 22:23:39 +0800

    add Exponents

---
+++ src/operators/Exponents.java
@@ -0,0 +1,17 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/9
+ */
+public class Exponents {
+    public static void main(String[] args) {
+        // Uppercase and lowercase ‘e’ are the same:
+        float expFloat = 1.39e-43f;
+        expFloat = 1.39E-43f;
+        System.out.println(expFloat);
+        double expDouble = 47e47d; // ‘d’ is optional
+        double expDouble2 = 47e47; // Automatically double
+        System.out.println(expDouble);
+    }
+}
