f63f889d7cf224b204032c1916bab7bacdbd5e9d
leiziqin <ziqinlei@gmail.com>
2018-06-10 22:26:48 +0800

    add Casting

---
+++ src/operators/Casting.java
@@ -0,0 +1,17 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/10
+ */
+public class Casting {
+    public static void main(String[] args) {
+        int i = 200;
+        long lng = (long) i;
+        lng = i; // "Widening," so cast not really required
+        long lng2 = (long) 200;
+        lng2 = 200;
+        // A "narrowing conversion":
+        i = (int) lng2; // Cast required
+    }
+}
