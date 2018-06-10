c4d2659dbef3df505139e91d52575989489b3af3
leiziqin <ziqinlei@gmail.com>
2018-06-10 22:27:59 +0800

    add CastingNumbers

---
+++ src/operators/CastingNumbers.java
@@ -0,0 +1,16 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/10
+ */
+public class CastingNumbers {
+    public static void main(String[] args) {
+        double above = 0.7, below = 0.4;
+        float fabove = 0.7f, fbelow = 0.4f;
+        System.out.println("(int)above: " + (int) above);
+        System.out.println("(int)below: " + (int) below);
+        System.out.println("(int)fabove: " + (int) fabove);
+        System.out.println("(int)fbelow: " + (int) fbelow);
+    }
+}
