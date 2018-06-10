d64e43f9c1486e516eb485d5c3c1ca64819247bf
leiziqin <ziqinlei@gmail.com>
2018-06-10 22:21:48 +0800

    add TernaryIfElse

---
+++ src/operators/TernaryIfElse.java
@@ -0,0 +1,26 @@
+package operators;
+
+/**
+ * @author Leiziqin
+ * @since 2018/6/10
+ */
+public class TernaryIfElse {
+    private static int ternary(int i) {
+        return i < 10 ? i * 100 : i * 10;
+    }
+
+    private static int standardIfElse(int i) {
+        if (i < 10) {
+            return i * 100;
+        } else {
+            return i * 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ternary(9));
+        System.out.println(ternary(10));
+        System.out.println(standardIfElse(9));
+        System.out.println(standardIfElse(10));
+    }
+}
