4e00b50c6aaea1cbcf8fc26f58275efe9994acc5
ziqin.lei <ziqin.lei@yingzt.com>
2018-06-11 17:23:38 +0800

    add ForEachString

---
+++ src/control/ForEachString.java
@@ -0,0 +1,13 @@
+package control;
+
+/**
+ * @author ziqin.lei
+ * @since 2018/6/11
+ */
+public class ForEachString {
+    public static void main(String[] args) {
+        for (char c : "An African Swallow".toCharArray()) {
+            System.out.print(c + " ");
+        }
+    }
+}
