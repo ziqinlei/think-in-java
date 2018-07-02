78f9e0dbd79c4df04643ded32a1840883e2e2378
leiziqin <ziqinlei@gmail.com>
2018-07-02 22:16:10 +0800

    Add OverloadingOrder

---
+++ src/initialization/OverloadingOrder.java
@@ -0,0 +1,20 @@
+package initialization;
+
+/**
+ * @author Leiziqin
+ * @since 2018/7/2
+ */
+public class OverloadingOrder {
+    static void f(String s, int i) {
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void f(int i, String s) {
+        System.out.println("int: " + i + ", String: " + s);
+    }
+
+    public static void main(String[] args) {
+        f("String first", 11);
+        f(99, "Int first");
+    }
+}
