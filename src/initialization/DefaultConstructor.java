e8e27ea5196b84b623f98321b2f4e25f3d782d1d
leiziqin <ziqinlei@gmail.com>
2018-07-02 22:26:33 +0800

    Add DefaultConstructor

---
+++ src/initialization/DefaultConstructor.java
@@ -0,0 +1,14 @@
+package initialization;
+
+/**
+ * @author Leiziqin
+ * @since 2018/7/2
+ */
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        new Bird();
+    }
+}
+
+class Bird {
+}
