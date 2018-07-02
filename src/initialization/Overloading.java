1bbc26a46eca4ab66d9dbe19b94a9549469ffd9b
leiziqin <ziqinlei@gmail.com>
2018-07-02 22:13:14 +0800

    Add Overloading

---
+++ src/initialization/Overloading.java
@@ -0,0 +1,38 @@
+package initialization;
+
+/**
+ * @author Leiziqin
+ * @since 2018/7/2
+ */
+public class Overloading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree tree = new Tree(i);
+            tree.info();
+            tree.info("Overloaded method");
+        }
+        new Tree();
+    }
+}
+
+class Tree {
+    int height;
+
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.println("Creating new Tree that is " + height + " feet tall");
+    }
+
+    void info() {
+        System.out.println("Tree is " + height + " feet tall");
+    }
+
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
+    }
+}
