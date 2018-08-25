7339953a483884cd4408a6094bcbc392ed66cf6f
leiziqin <ziqinlei@gmail.com>
2018-08-25 17:49:11 +0800

    Add FilledList

---
+++ src/typeinfo/FilledList.java
@@ -0,0 +1,43 @@
+package typeinfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Leiziqin
+ * @since 2018/8/25
+ */
+public class FilledList<T> {
+    private Class<T> type;
+
+    public FilledList(Class<T> type) {
+        this.type = type;
+    }
+
+    public List<T> create(int nElements) {
+        List<T> result = new ArrayList<>();
+        try {
+            for (int i = 0; i < nElements; i++) {
+                result.add(type.newInstance());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FilledList<CountedInteger> fl = new FilledList<>(CountedInteger.class);
+        System.out.println(fl.create(15));
+    }
+}
+
+class CountedInteger {
+    private static long counter;
+    private final long id = counter++;
+
+    @Override
+    public String toString() {
+        return Long.toString(id);
+    }
+}
